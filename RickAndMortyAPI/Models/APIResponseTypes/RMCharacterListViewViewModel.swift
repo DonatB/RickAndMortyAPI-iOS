cc112df4b4b2ff38cf31b19d912cb7bdc733a664
Donat-Noexis <99733619+Donat-Noexis@users.noreply.github.com>
2023-01-26 16:50:32 +0100

    ScrollView to bottom and spinner implementation
    Utilized the scrollView function of scrollViewDidScroll() to check whether the user is at the bottom of the collectionView. Also we've implemented a spinner which is being shown as a UICollectionReusableView, and it's implemented almost the same way by dequeueing it like a normal cell.

--- RickAndMortyAPI/Models/APIResponseTypes/RMCharacterListViewViewModel.swift
+++ RickAndMortyAPI/Models/APIResponseTypes/RMCharacterListViewViewModel.swift
@@ -18,6 +18,8 @@ final class RMCharacterListViewViewModel: NSObject {
     
     public weak var delegate: RMCharacterListViewViewModelDelegate?
     
+    private var isLoadingMoreCharacters = false
+    
     private var characters: [RMCharacter] = [] {
         didSet {
             for character in characters {
@@ -52,11 +54,11 @@ final class RMCharacterListViewViewModel: NSObject {
     
     /// Paginate if additional characters are needed
     public func fetchAdditionalCharacters() {
-        
+        isLoadingMoreCharacters = true
     }
     
     public var shouldShowLoadMoreIndicator: Bool {
-        return apiInfo?.next != nil
+        apiInfo?.next != nil
     }
     
 }
@@ -73,6 +75,23 @@ extension RMCharacterListViewViewModel: UICollectionViewDataSource, UICollection
         let viewModel = cellViewModels[indexPath.row]
         cell.configure(with: viewModel)
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionView.elementKindSectionFooter,
+              let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: RMFooterLoadingCollectionReusableView.identifier, for: indexPath) as? RMFooterLoadingCollectionReusableView  else {
+            fatalError("Unsupported")
+        }
+        
+        footer.startAnimating()
+        return footer
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        guard shouldShowLoadMoreIndicator else {
+            return .zero
+        }
+        return CGSize(width: collectionView.frame.width, height: 100)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -94,10 +113,21 @@ extension RMCharacterListViewViewModel: UICollectionViewDataSource, UICollection
 
 extension RMCharacterListViewViewModel: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        guard shouldShowLoadMoreIndicator else {
+        guard shouldShowLoadMoreIndicator, !isLoadingMoreCharacters else {
             return
         }
         
+        let offset = scrollView.contentOffset.y
+        let totalContentHeight = scrollView.contentSize.height
+        let totalScrollViewFixedHeight = scrollView.frame.size.height
+        
+//        print("Offset: \(offset)")
+//        print("totalContentHeight: \(totalContentHeight)")
+//        print("totalScrollViewFixedHeight: \(totalScrollViewFixedHeight)")
+        
+        if offset >= (totalContentHeight - totalScrollViewFixedHeight - 80) {
+            fetchAdditionalCharacters()
+        }
         
     }
 }
