525f6265f6244fc4bf7bbbcaff33582c994d8713
Donat-Noexis <99733619+Donat-Noexis@users.noreply.github.com>
2023-01-30 10:48:51 +0100

    Setting up collectionView for character detail screen.

--- RickAndMortyAPI/Controllers/Other/RMCharacterDetailViewController.swift
+++ RickAndMortyAPI/Controllers/Other/RMCharacterDetailViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 final class RMCharacterDetailViewController: UIViewController {
     private var viewModel: RMCharacterDetailViewViewModel
     
+    private let detailView = RMCharacterDetailView()
+    
+    //MARK: - Init
+    
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -26,7 +30,25 @@ final class RMCharacterDetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = viewModel.title
+        view.addSubview(detailView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapShare))
+        addConstraints()
+//        viewModel.fetchCharacterData()
     }
-
+    
+    @objc private func didTapShare() {
+        print("Tapping sheR")
+    }
+    
+    private func addConstraints() {
+        
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            detailView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+    }
 
 }
