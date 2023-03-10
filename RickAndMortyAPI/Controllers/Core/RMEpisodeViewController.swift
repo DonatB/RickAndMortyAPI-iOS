546c192ef5e1f79d87dcadc4ad695992f999a9b4
Donat-Noexis <99733619+Donat-Noexis@users.noreply.github.com>
2023-03-10 11:53:51 +0100

    Creating new classes for episode tab and copy-pasta fixes.

--- RickAndMortyAPI/Controllers/Core/RMEpisodeViewController.swift
+++ RickAndMortyAPI/Controllers/Core/RMEpisodeViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 /// Controller to show and search for Episodes
 final class RMEpisodeViewController: UIViewController {
 
+    private let episodeListView = RMEpisodeListView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCharacterListView()
+    }
+    
+    func configureCharacterListView() {
         view.backgroundColor = .systemBackground
         title = "Episodes"
+        episodeListView.delegate = self
+        view.addSubview(episodeListView)
+        
+        NSLayoutConstraint.activate([
+            episodeListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            episodeListView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            episodeListView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            episodeListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
     }
 
-
 }
+
+//MARK: - EpisodeListViewDelegate
+extension RMEpisodeViewController: RMEpisodeListViewDelegate {
+    func rmEpisodeListView(_ characterListView: RMEpisodeListView, didSelectEpisode episode: RMEpisode) {
+        // Open detail controller for that episode
+        let detailVC = RMEpisodeDetailViewController(url: URL(string: episode.url))
+        detailVC.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+    
+}
+
+
---
+++ RickAndMortyAPI/Views/Episode/RMEpisodeListView.swift
@@ -0,0 +1,106 @@
+//
+//  RMEpisodeListView.swift
+//  RickAndMortyAPI
+//
+//  Created by Donat Bajrami on 10.3.23.
+//
+
+
+import UIKit
+
+protocol RMEpisodeListViewDelegate: AnyObject {
+    func rmEpisodeListView(_ characterListView: RMEpisodeListView, didSelectEpisode episode: RMEpisode)
+}
+
+/// View that handles list of episodes, loader, etc.
+final class RMEpisodeListView: UIView {
+    
+    public weak var delegate: RMEpisodeListViewDelegate?
+    
+    private let viewModel = RMCharacterListViewViewModel()
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isHidden = true
+        collectionView.alpha = 0
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(RMCharacterCollectionViewCell.self,
+                                forCellWithReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier)
+        collectionView.register(RMFooterLoadingCollectionReusableView.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
+                                withReuseIdentifier: RMFooterLoadingCollectionReusableView.identifier)
+        
+        return collectionView
+    }()
+    
+    //MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubviews(collectionView, spinner)
+        
+        configureConstraints()
+        
+        spinner.startAnimating()
+        viewModel.delegate = self
+        viewModel.fetchCharacters()
+        setupCollectionView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    private func setupCollectionView() {
+        collectionView.dataSource = viewModel
+        collectionView.delegate = viewModel
+    }
+    
+}
+
+extension RMEpisodeListView: RMEpisodeListViewViewModel {
+    func didLoadMoreCharacters(with newIndexPaths: [IndexPath]) {
+        collectionView.performBatchUpdates {
+            self.collectionView.insertItems(at: newIndexPaths)
+        }
+    }
+    
+    func didSelectCharacter(_ character: RMCharacter) {
+        delegate?.rmCharacterListView(self, didSelectCharacter: character)
+    }
+    
+    func didLoadInitialCharacters() {
+        spinner.stopAnimating()
+        collectionView.isHidden = false
+        collectionView.reloadData()
+        
+        UIView.animate(withDuration: 0.4, delay: 0) {
+            self.collectionView.alpha = 1
+        }
+    }
+}
