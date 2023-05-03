7f23709bdb4b105e9df463ca86aa639032724921
Donat-Noexis <99733619+Donat-Noexis@users.noreply.github.com>
2023-05-03 11:28:39 +0200

    Implement settings view models and options
    Added view models and options for the settings screen, including icons, display titles, and colors. Implemented Identifiable and Hashable protocols for easier integration with SwiftUI.

---
+++ RickAndMortyAPI/ViewModels/Settings/RMSettingsViewViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMortyAPI
+//
+//  Created by Donat Bajrami on 2.5.23.
+//
+
+import Foundation
+
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+}
